b123c969973f05a8b40f67aded5f075d75dbe3d9
Timothy Sprague <Tjsprague@fullsail.edu>
2016-08-30 15:56:22 -0400

    Added PlayerHealth Script

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int playerCurrentHealth;
+    public int playerMaxHealth;
+    public bool isAlive = true;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void DecreaseHealth(int _value)
+    {
+        if (isAlive)
+        {
+            playerCurrentHealth -= _value;
+        }
+        if (playerCurrentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void IncreaseHealth(int _value)
+    {
+        if (isAlive)
+        {
+            playerCurrentHealth += _value;
+        }
+    }
+
+
+    void Death()
+    {
+        isAlive = false;
+    }
+}

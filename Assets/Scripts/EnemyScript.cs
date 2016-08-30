c107f206963cdfb8c51593ebc25b8410c6aba471
Daniel Banowetz <dabanowetz@fullsail.edu>
2016-08-30 15:59:50 -0400

    Starting EnemyScript

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+    
+   
+    public int health;
+    public bool alive;
+    public int attack;
+    public int defense;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

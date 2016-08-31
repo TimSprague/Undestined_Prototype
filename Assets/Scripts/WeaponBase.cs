5088d4a1e8ca411bb969f5ef26d4817b35d0771b
Daniel Banowetz <dabanowetz@fullsail.edu>
2016-08-31 15:45:13 -0400

    Starting a base for weapons

---
+++ Assets/Scripts/WeaponBase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponBase : MonoBehaviour {
+
+
+    public int damage;
+    public float durability;
+    public float attackSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+}

81aaa6c98962ba5f9986a9d47917e5bf2b1b9556
Pedro De Jesus <pdejesus01@outlook.com>
2016-09-02 16:54:32 -0400

    Worked on light attack and heavy attack.  Also changed some of the woods.

---
+++ Assets/Scripts/MeleeAttack.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeAttack : MonoBehaviour {
+
+    public float vSpeed = 2.0f;
+    public float swordTurn = 2.0f;
+    public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //float v = vSpeed * Input.GetAxis("Mouse X");
+        //transform.Translate(0, 0, 0);
+
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+
+        if (Input.GetMouseButton(0))
+        {
+            transform.Rotate(0, 0, -90 * Time.deltaTime);
+            //transform.Translate(Vector3.left * Time.deltaTime * swordTurn);
+        }
+
+        if(Input.GetMouseButton(1))
+        {
+            transform.Rotate(90 * Time.deltaTime, 0, 0);
+            //transform.Translate(Vector3.right * Time.deltaTime * swordTurn);
+        }
+
+	}
+}

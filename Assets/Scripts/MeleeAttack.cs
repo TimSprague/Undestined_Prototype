64c1ba4d11ca5ef6eb38a89dd911fd910cd342bd
Pedro De Jesus <pdejesus01@outlook.com>
2016-09-05 15:28:46 -0400

    Finished sword attack and it's animations.

--- Assets/Scripts/MeleeAttack.cs
+++ Assets/Scripts/MeleeAttack.cs
@@ -7,29 +7,29 @@ public class MeleeAttack : MonoBehaviour {
     public float swordTurn = 2.0f;
     public float speed = 10.0f;
 
+    public Animator swordAnimation;
+
 	// Use this for initialization
 	void Start () {
+
+        swordAnimation = GetComponent<Animator>();
         
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        //float v = vSpeed * Input.GetAxis("Mouse X");
-        //transform.Translate(0, 0, 0);
-
+        
         transform.Rotate(Vector3.up, speed * Time.deltaTime);
 
         if (Input.GetMouseButton(0))
         {
-            transform.Rotate(0, 0, -90 * Time.deltaTime);
-            //transform.Translate(Vector3.left * Time.deltaTime * swordTurn);
+            swordAnimation.Play("LightAttack");
         }
 
         if(Input.GetMouseButton(1))
         {
-            transform.Rotate(90 * Time.deltaTime, 0, 0);
-            //transform.Translate(Vector3.right * Time.deltaTime * swordTurn);
+            swordAnimation.Play("HeavyAttack");
         }
 
 	}

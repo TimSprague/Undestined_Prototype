1915759b43e8f3b5f13fe246cac98639a9d4c3c0
Daniel Banowetz <dabanowetz@fullsail.edu>
2016-09-08 11:04:25 -0400

    fixing things with enemyAnim

--- Assets/Scripts/ComboStates.cs
+++ Assets/Scripts/ComboStates.cs
@@ -60,7 +60,7 @@ public class ComboStates : MonoBehaviour {
                         Vector3 temp = player.TransformDirection(-player.transform.right);
                         Vector3 vel = other.rigidBody.velocity;
                         other.rigidBody.velocity = new Vector3(0,vel.y,vel.z*-.07f);
-                        other.rigidBody.AddForce(new Vector3(0, 550, 80) );
+                        other.rigidBody.AddForce(new Vector3(0, 5000, 80) );
 
                         other.knockUp();
                     }
@@ -81,8 +81,7 @@ public class ComboStates : MonoBehaviour {
                         Vector3 temp = player.TransformDirection(-player.transform.right);
                         Vector3 vel = other.rigidBody.velocity;
                         other.rigidBody.velocity = new Vector3(0, -vel.y*5, vel.z*-.1f);
-                        other.rigidBody.AddForce(new Vector3(0, -800, 800
-                            ) );
+                        other.rigidBody.AddForce(new Vector3(0, -700*temp.y, 1000  *temp.z ) );
                     }
                 }
                 break;
@@ -98,11 +97,13 @@ public class ComboStates : MonoBehaviour {
                     /**double slash with knock back
                     Call function
                        **/
-                    Vector3 temp = player.TransformDirection(-other.transform.forward);
-                    Vector3 vel = other.rigidBody.velocity;
-                    other.rigidBody.velocity = new Vector3(0, vel.y, vel.z * -25);
-                    other.rigidBody.AddForce(new Vector3(0, 100, 8000));
-
+                    if (other != null)
+                    {
+                        Vector3 temp = player.TransformDirection(-other.transform.forward);
+                        Vector3 vel = other.rigidBody.velocity;
+                        other.rigidBody.velocity = new Vector3(0, vel.y, vel.z * -25);
+                        other.rigidBody.AddForce(new Vector3(0, 100, 8000));
+                    }
                 }
                 break;
             case (int)COMBOSTATE.lightHeavyJump:

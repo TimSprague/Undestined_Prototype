b343902174cb03084ee4881f3b71927d123e9f4d
Timothy Sprague <Tjsprague@fullsail.edu>
2016-09-08 00:06:45 -0400

    Added Camera script that allows the following of the char, implemented jumping, still working on making it consistent and not allowing double jump to start with.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,85 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public class PlayerMovement : MonoBehaviour {
 
-public class PlayerMovement : MonoBehaviour
-{
-
+    public float moveSpeed;
     public Transform cameraTransform;
+    public float forwardVel;
+    public float rotateVel;
+    Quaternion targetRotation;
     Rigidbody rb;
-
-    public float speed;
-    public float turnSpeed;
-
-    bool jumping = false;
-    public int jumpHeight = 8;
-
-    CharacterController cc;
-    Quaternion targRotation;
-    float forwardInput, turnInput;
-
+    public float jumpHeight;
+    public bool jumping;
     public Quaternion TargetRotation
     {
-        get { return targRotation; }
+        get { return targetRotation; }
     }
-    // Use this for initialization
-    void Start()
-    {
-        // playerTransform = GetComponent<Transform>();
-        // rb.isKinematic = false;
-        cc = GetComponent<CharacterController>();
+	// Use this for initialization
+	void Start () {
 
-        targRotation = transform.rotation;
+        targetRotation = transform.rotation;
         if (GetComponent<Rigidbody>())
         {
             rb = GetComponent<Rigidbody>();
         }
-        else
-            Debug.LogError("Character needs RigidBody");
-        forwardInput = turnInput = 0;
-    }
-
-    void GetInput()
+        jumping = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
     {
-        forwardInput = Input.GetAxis("Vertical");
-        turnInput = Input.GetAxis("Horizontal");
-    }
-
-    void Turn()
-    {
-        // multiplying adds to the last roation
-        targRotation *= Quaternion.AngleAxis(turnSpeed * turnInput * Time.deltaTime, Vector3.up);
-        transform.rotation = targRotation;
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        GetInput();
         Turn();
-    }
+	}
 
     void FixedUpdate()
     {
         Move();
     }
 
-    void Move()
+    void Turn()
     {
-        //rb.velocity = cameraTransform.forward * forwardInput * speed;
-
-        Vector3 moveForward = Input.GetAxis("Vertical") * transform.TransformDirection(Vector3.forward) * speed;
-        // rotate
-        // transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0));
-        cc.Move(moveForward * Time.deltaTime);
-
-        //playerTransform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f, Input.GetAxis("Vertical") * speed * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            rb.velocity = new Vector3(0, jumpHeight, 0);
-        }
-
+        // multiplying rotation is adding to previous rotation
+        targetRotation *= Quaternion.AngleAxis(rotateVel * Input.GetAxis("Horizontal") * Time.deltaTime, Vector3.up);
+        transform.rotation = targetRotation;
     }
 
+    void Move()
+    {
 
+        rb.velocity = transform.forward * Input.GetAxis("Vertical") * forwardVel;
+
+        if (Input.GetButtonDown("Jump") && !jumping)
+        {
+            rb.velocity = new Vector3(0, jumpHeight, 0);
+            jumping = true;
+        }
+
+        rb.velocity += (0.5f * Physics.gravity);
+        
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+        if (col.collider.tag == "Terrain")
+        {
+        }
+            jumping = false;
+    }
 }

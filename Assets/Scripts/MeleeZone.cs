c81f2d6eef2543a17c8bff96422c07a52ff040cd
Pedro De Jesus <pdejesus01@outlook.com>
2016-09-12 14:11:28 -0400

    Got rid of Ethan.

--- Assets/Scripts/MeleeZone.cs
+++ Assets/Scripts/MeleeZone.cs
@@ -29,6 +29,7 @@ public class MeleeZone : MonoBehaviour {
         {
 
             if (Input.GetButton("Fire1"))
+            //if (Input.GetMouseButton(0))
             {
                 if (!attacking)
                 {
@@ -85,14 +86,15 @@ public class MeleeZone : MonoBehaviour {
                 {
 
                     Vector3 temp = playerTrans.forward;
-                    enemScript.rigidBody.AddForce(new Vector3(temp.x * 25, 0, temp.z * 100));
+                    enemScript.rigidBody.AddForce(new Vector3(temp.x * 250, 0, temp.z * 100));
                     enemScript.TakeDmg(5);
+                    lightAtk = false;
                 }
                 if (heavyAtk)
                 {
                     Vector3 temp = playerTrans.forward;
                    // enemScript.rigidBody.velocity = new Vector3(temp.normalized.x*5, temp.normalized.y, temp.normalized.z*5);
-                    enemScript.rigidBody.AddForce(new Vector3(temp.normalized.x*5, 110, temp.normalized.z*7.5f));
+                    enemScript.rigidBody.AddForce(new Vector3(temp.normalized.x*5, 120, temp.normalized.z*7.5f));
                     enemScript.knockedUp = true;
                     enemScript.TakeDmg(10);
 
@@ -103,7 +105,7 @@ public class MeleeZone : MonoBehaviour {
         {
             lightAtk = heavyAtk = attacking = false;
         }
-        lightAtk = heavyAtk = false;
+        lightAtk = heavyAtk=attacking = false;
 
 
     }

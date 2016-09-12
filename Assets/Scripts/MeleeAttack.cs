db99f82fc8387b30b70b6b4615a3c8606f84a4a9
Lucas Carver <lcarverstudent@yahoo.com>
2016-09-12 11:33:30 -0400

    Testing combo animation in MeleeAttack.cs (Not permanent place for animation execution)

--- Assets/Scripts/MeleeAttack.cs
+++ Assets/Scripts/MeleeAttack.cs
@@ -6,6 +6,8 @@ public class MeleeAttack : MonoBehaviour {
     public float vSpeed = 2.0f;
     public float swordTurn = 2.0f;
     public float speed = 10.0f;
+    [SerializeField] float comboTime = 0.0f;
+    [SerializeField] int currentCombo = 0;
 
     public Animator swordAnimation;
     [SerializeField] AudioSource sfxSource;
@@ -22,19 +24,49 @@ public class MeleeAttack : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-        if (Input.GetButton("Fire1"))
+        //if(comboTime > 0)
+        //{
+        //    comboTime -= Time.deltaTime;
+        //    if (comboTime <= 0)
+        //    {
+        //        comboTime = 0;
+        //        currentCombo = 0;
+        //    }
+        //}
+        if (Input.GetButtonDown("Fire1"))
         {
+            //if (currentCombo == 0 || comboTime == 0)
+            //{
+            //    if(!swordAnimation.IsInTransition(0))
+            //    {
+            //        swordAnimation.Play("LightAttack");
+            //        currentCombo++;
+            //    }
+            //}
+            //else if(currentCombo == 1 && comboTime > 0)
+            //{
+            //    if (!swordAnimation.IsInTransition(0))
+            //    {
+            //        swordAnimation.Play("LightAttack2");
+            //        currentCombo++;
+            //    }
+            //}
+            //else if(currentCombo == 2 && comboTime > 0)
+            //{
+            //    if (!swordAnimation.IsInTransition(0))
+            //    {
+            //        currentCombo = 0;
+            //        swordAnimation.Play("LightAttack3");
+            //    }
+            //}
+            //comboTime = 1.0f;
+
             swordAnimation.Play("LightAttack");
-           
         }
 
         if(Input.GetButton("Fire2"))
         {
             swordAnimation.Play("HeavyAttack");
-           
-        
-
         }
 
     }

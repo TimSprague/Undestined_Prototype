6187b28aec6a9fe39ac7de40b451e6a3682f6260
Pedro De Jesus <pdejesus01@outlook.com>
2016-09-13 16:27:32 -0400

    Worked on enemy spawner and enemy counter.

---
+++ Assets/Scripts/EnemyCount.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EnemyCount : MonoBehaviour {
+
+    // Enemy Counter
+    private int count;
+    public Text countText;
+    Vector3 spawnLocation;
+    bool enemy;
+
+    // Use this for initialization
+    void Start () {
+        count = 0;
+        enemy = GameObject.Find("Enemy").GetComponent<EnemyScript>().alive;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (enemy == false)
+        {
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = count.ToString() + " / 10";
+    }
+}

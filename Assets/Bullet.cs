91994bcdec949d9e9aa496071868a3947eb346b1
Daniel Banowetz <dabanowetz@fullsail.edu>
2016-09-16 13:06:00 -0400

    Working on Enemies

---
+++ Assets/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+    bool alive;
+    float aliveTimer;
+	// Use this for initialization
+	void Start () {
+        aliveTimer = 2;
+        alive = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(alive)
+        {
+            aliveTimer -= Time.deltaTime;
+            if(aliveTimer<0)
+            {
+                DestroyImmediate(this.gameObject);
+            }
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.GetComponent<PlayerHealth>().DecreaseHealth(5);
+
+            Destroy(this.gameObject);
+        }
+    }
+}
